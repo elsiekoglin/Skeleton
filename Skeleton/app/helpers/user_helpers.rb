eaaceff0b54bdf017afe21ac80b57fde8175d38f
Elsie Koglin <elsiekoglin@gmail.com>
2017-04-11 10:04:04 -0500

    Update user_helpers.rb

--- Skeleton/app/helpers/user_helpers.rb
+++ Skeleton/app/helpers/user_helpers.rb
@@ -1,18 +1,25 @@
 module UserFormatting
-
   def logged_in?
     !!session[:user_id]
   end
-
-  # ADJUST ACCORDINGLY
-  # def current_user
-  #   @user ||= User.find_by(id: session[:user_id]if logged_in?
-  # end
-
-  # def author?(user_id)
-  #   session[:user_id] == user_id
-  # end
-
+  
+  def current_user
+    User.find_by(id: session[:user_id]) if logged_in?
+  end
+  
+  def author?(user_id)
+    session[:user_id] == user_id
+  end
+  
+#   def subscribed?(channel_id)
+#    user = User.find_by(id: session[:user_id])
+#    user.subscriptions.each do  |subscription| 
+#      if subscription.channel.id == channel_id
+#        return true
+#      end
+#    end
+#    return false
+#  end
 end
 
 helpers UserFormatting
